3bf385417b2f361cfe6c8be53ff72e5c2eda75f1
niraj1412 <14nirajkumar@gmail.com>
2024-05-11 14:10:41 +0530

    ggg

--- app/page.js
+++ app/page.js
@@ -44,6 +44,13 @@ function Page() {
 
   return (
     <div>
+    <div className="overflow-hidden">
+            <marquee behavior="scroll" direction="left" scrollamount="8" >
+                <h1 className="text-[19px]">
+                    🎉 <span className="text-[#9A391D] text-[20px] font-bold"> Admission Open </span> for the coming <span className="text-[#9A391D] text-[20px] font-bold">2024-25</span> session 🎉
+                </h1>
+            </marquee>
+        </div>
      <div style={{ width: '560px', height: '315px', float: 'none', clear: 'both', margin: '2px auto' }}>
         <embed
           src="https://www.youtube.com/embed/J---aiyznGQ?autohide=1&autoplay=1"
