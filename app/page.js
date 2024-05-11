413e30afc2eee4c6adffcb49161a948ca514b0f5
niraj1412 <14nirajkumar@gmail.com>
2024-05-11 14:32:10 +0530

    k

--- app/page.js
+++ app/page.js
@@ -1,47 +1,7 @@
-"use client";
-import React, { useEffect, useRef } from "react";
+
+import React from "react";
 
 function Page() {
-  const playerRef = useRef(null);
-
-  useEffect(() => {
-    // Load the YouTube Player API script
-    const script = document.createElement("script");
-    script.src = "https://www.youtube.com/iframe_api";
-    document.body.appendChild(script);
-
-    // Initialize the YouTube Player when the API is ready
-    window.onYouTubeIframeAPIReady = () => {
-      playerRef.current = new window.YT.Player("youtube-player", {
-        videoId: "J---aiyznGQ",
-        playerVars: {
-          autoplay: 1,
-          autohide: 1,
-          controls: 0,
-          modestbranding: 1,
-          loop: 1,
-          playlist: "J---aiyznGQ",
-          enablejsapi: 1,
-          origin: window.location.origin,
-        },
-        events: {
-          onReady: onPlayerReady,
-        },
-      });
-    };
-
-    // Clean up
-    return () => {
-      document.body.removeChild(script);
-      window.onYouTubeIframeAPIReady = null;
-    };
-  }, []);
-
-  // When the player is ready, start playback
-  const onPlayerReady = (event) => {
-    event.target.playVideo();
-  };
-
   return (
     <div>
     <div className="overflow-hidden">
@@ -51,12 +11,14 @@ function Page() {
                 </h1>
             </marquee>
         </div>
-     
-      <div
-        id="youtube-player"
-        style={{ width: "560px", height: "315px", margin: "2px auto" }}
-      ></div>
-      
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/J---aiyznGQ?autoplay=1"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 }
