cfcea53eaf5609389f551f9b45b22213a683f1a7
niraj1412 <14nirajkumar@gmail.com>
2024-05-11 14:37:45 +0530

    s

--- app/page.js
+++ app/page.js
@@ -7,18 +7,23 @@ function Page() {
     <div className="overflow-hidden">
             <marquee behavior="scroll" direction="left" scrollamount="8" >
                 <h1 className="text-[19px]">
-                    🎉 <span className="text-[#9A391D] text-[20px] font-bold"> Admission Open </span> for the coming <span className="text-[#9A391D] text-[20px] font-bold">2024-25</span> session 🎉
+                    🎉 <span className="text-[#9A391D] text-[20px] font-bold"> das Open </span> for the coming <span className="text-[#9A391D] text-[20px] font-bold">2024-25</span> session 🎉
                 </h1>
             </marquee>
         </div>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/J---aiyznGQ?autoplay=1"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+     <div style={{ width: '560px', height: '315px', float: 'none', clear: 'both', margin: '2px auto' }}>
+        <embed
+          src="https://www.youtube.com/embed/J---aiyznGQ?autohide=1&autoplay=1"
+          wmode="transparent"
+          type="video/mp4"
+          width="100%" height="100%"
+          allow="autoplay; encrypted-media; picture-in-picture"
+          allowFullScreen
+          title="Keyboard Cat"
+        />
+      </div>
+
+      
     </div>
   );
 }
